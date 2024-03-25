b65716f598742a881b2604a085059220ec410014
Emjnes <kayjone127@gmail.com>
2024-03-24 23:40:11 -0400

    array

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,105 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Random;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class Lab_09_ArrayStuff {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // Declare and initialize the array
+        int[] dataPoints = new int[100];
+
+        // Populate the array with random values between 1 and 100
+        Random rnd = new Random();
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rnd.nextInt(100) + 1; // Generates a random integer between 1 and 100 (inclusive)
         }
+
+        // Display the values in the array separated by " | "
+        System.out.print("Values in dataPoints array: ");
+        for (int i = 0; i < dataPoints.length; i++) {
+            System.out.print(dataPoints[i]);
+            if (i < dataPoints.length - 1) {
+                System.out.print(" | ");
+            }
+        }
+        System.out.println(); // Move to the next line
+
+        // Calculate the sum and average of the values in the array
+        int sum = 0;
+        for (int dataPoint : dataPoints) {
+            sum += dataPoint;
+        }
+        double average = (double) sum / dataPoints.length;
+
+        // Output the sum and average
+        System.out.println("The sum of the random array dataPoints is: " + sum);
+        System.out.printf("The average of the random array dataPoints is: %.2f%n", average);
+
+        // Prompt user for input
+        int userValue = getRangedInt("Enter an integer between 1 and 100: ", 1, 100);
+
+        // Count how many times the user's value is found within the array
+        int count = 0;
+        for (int value : dataPoints) {
+            if (value == userValue) {
+                count++;
+            }
+        }
+        System.out.println("The user's value " + userValue + " was found " + count + " times in the array.");
+
+        // Prompt user again for input
+        userValue = getRangedInt("Enter another integer between 1 and 100: ", 1, 100);
+
+        // Find the position of the user's value in the array
+        int position = -1;
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] == userValue) {
+                position = i;
+                break; // exit the loop when the value is found
+            }
+        }
+        if (position != -1) {
+            System.out.println("The value " + userValue + " was found at array index " + position);
+        } else {
+            System.out.println("The value " + userValue + " was not found in the array.");
+        }
+
+        // Find minimum and maximum values in the array
+        int min = dataPoints[0];
+        int max = dataPoints[0];
+        for (int i = 1; i < dataPoints.length; i++) {
+            if (dataPoints[i] < min) {
+                min = dataPoints[i];
+            }
+            if (dataPoints[i] > max) {
+                max = dataPoints[i];
+            }
+        }
+        System.out.println("Minimum value in the array: " + min);
+        System.out.println("Maximum value in the array: " + max);
+
+        // Calculate and display the average of dataPoints
+        System.out.println("Average of dataPoints is: " + getAverage(dataPoints));
+    }
+
+    public static int getRangedInt(String prompt, int low, int high) {
+        int intValue;
+        do {
+            System.out.print(prompt + " [" + low + " - " + high + "]: ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Invalid input! Please enter an integer.");
+                scanner.next(); // discard non-integer input
+            }
+            intValue = scanner.nextInt();
+        } while (intValue < low || intValue > high);
+        return intValue;
+    }
+
+    public static double getAverage(int[] values) {
+        int sum = 0;
+        for (int value : values) {
+            sum += value;
+        }
+        return (double) sum / values.length;
     }
 }
--- src/SafeInput/getRangedInt.java
+++ src/SafeInput/getRangedInt.java
@@ -1,4 +1,34 @@
 package SafeInput;
 
-public class getRanged {
+import java.util.Scanner;
+
+import static java.lang.reflect.Array.getInt;
+
+class Lab_09_ArrayStuff {
+    public static void main(String[] args) {
+        // Your main method code here
+    }
+
+    /**
+     * Prompts the user to input an integer within a specified inclusive range.
+     *
+     * @param pipe    a Scanner opened to read from System.in
+     * @param prompt  prompt for the user
+     * @param low     low value for the input range
+     * @param high    high value for the input range
+     * @return an integer within the specified range inputted by the user
+     */
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high) {
+        int intValue;
+        do {
+            intValue = getInt(pipe, prompt + " [" + low + " - " + high + "]");
+        } while (intValue < low || intValue > high);
+        return intValue;
+    }
+
+    private static int getInt(Scanner pipe, String s) {
+
+
+        return 0;
+    }
 }
